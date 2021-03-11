f50917124a9fa43ceffea244b264538751d00399
Butch <butchbet01@gmail.com>
2021-03-11 16:00:21 -0500

    Program 5, 6, 9 coded and 11 almost finished.

--- repetition/11.c
+++ repetition/11.c
@@ -6,20 +6,22 @@ int main() {
     float exam[3] = {0.90,0.80,0.85}; // porcentage for the exam of each subject
     float tasks[3] = {0.10,0.20,0.15}; // porcentage for each task of the subjects
     float n_exam[3]; // notes in each exam
-    int num_tasks[3] = {3, 2, 3}; // number of tasks of each subject
-    float n_tasks[3][3];
+    int num_tasks[3] = {3, 2, 3}, i=0, j=0; // number of tasks of each subject
+    float n_tasks[3][3]; // note tasks 3 subjects with a max of 3 index
+
+    float posi[3]; // positionate since lower to greater note
     
     // loop to require exam's notes
-    for (int i = 0; i < 3; i++){
+    for (i = 0; i < 3; i++){
         printf("\nEnter the exam note for %s: ", &subject[i][0]); scanf("%f",&n_exam[i]);
         exam[i] *= n_exam[i];
         printf("\n\n");
 }
     
     // loop to require the task notes for each subject and multiplicate with the asigned porcente 
-    for (int i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) {
         printf("Enter %s task notes\n", &subject[i][0]);
-        for (int j = 0; j < num_tasks[i]; j++) {
+        for (j = 0; j < num_tasks[i]; j++) {
             printf("--> ");scanf("%f", &n_tasks[i][j]);
             n_tasks[i][j] *= tasks[i];
             printf("\n");
@@ -27,19 +29,19 @@ int main() {
     }
     printf("\n");
     
-    
+     
     // loop to calculate the final note to each subject
-    for (int i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) {
         exam[i] += n_tasks[i][0];
-        for (int j = 1; j < num_tasks[i]; j++) {
+        for (j = 1; j < num_tasks[i]; j++) {
             exam[i] += n_tasks[i][j];
         }
     } 
     printf("\n\n");
     
     // loop to display the final note to each subject
-    for (int i = 0; i < 3; i++) {
-        if (exam[i] >= 5.0 || exam[i] == 5){
+    for (i = 0; i < 3; i++) {
+        if (exam[i] >= 5.0){
             exam[i] = 5.0;
             printf("The %s final note: %.1f. \n",&subject[i][0], exam[i]);
         }
@@ -47,6 +49,25 @@ int main() {
             printf("The %s final note: %.1f. \n",&subject[i][0], exam[i]);
         }
     }
-    
+
+//    // loop to know the lower note
+//    for (i = 0; i < 3; i++){
+//      if (exam[i] <= exam[i++] && exam[i++] != 0){
+//	posi[0] = exam[i];
+//      }else{
+//	posi[0] = exam[i++];
+//      }
+//    }
+//
+//    // loop to know the greater note
+//    for (i = 0; i < 3; i++){
+//      if (exam[i] <= exam[i++] && exam[i++] != 0){ // !=0 brecase if 0 and 1 index arent the greater then the last position will be the greater and viceversa  
+//	posi[1] = exam[i];
+//      }else{
+//	  posi[1] = exam[i++];
+//      }
+//    } 
+//
+//    printf("The lower note is: %.1f and greater is: %.1f\n", posi[0], posi[1]);
     return 0;
 }
