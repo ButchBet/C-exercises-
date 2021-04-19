6f9e2b853293946dc0c00b69d46cd3504e366005
Alejandro <k.salazar@utp.edu.co>
2021-04-19 15:22:32 -0500

    learnig to use structs with pointers

--- structurs/3.c
+++ structurs/3.c
@@ -10,24 +10,28 @@ void main(void){
         char b_type[3];
     }people[2];
 
+    struct regist *p_regist;
+    p_regist = people;
     
     // requiring the data for the users
     int i = 0;
     
     while(i < 2){
         printf("\n\nEnter id: ");
-        scanf("%li", &people[i].id );
+        scanf("%li", &p_regist->id );
         fflush(stdin); // Use this built-in function to can clear the buffer
         printf("\nEnter first name: ");
-        gets(people[i].name);
+        gets(p_regist->name);
         printf("\nEnter last name: ");
-        gets(people[i].last_name);
+        gets(p_regist->last_name);
         printf("\nEnter the email: ");
-        gets(people[i].email);
+        gets(p_regist->email);
         printf("\nEnter blood type b_type: ");
-        gets(people[i].b_type);
+        gets(p_regist->b_type);
+        p_regist++;
         i++;
     }
+    p_regist -= 2;
 
     i = 0;
 
@@ -35,11 +39,12 @@ void main(void){
     while(i < 2){
         printf("\n");
         printf("\n");
-        printf("%li\n", people[i].id );
-        puts(people[i].name);
-        puts(people[i].last_name);
-        puts(people[i].email);
-        puts(people[i].b_type);
+        printf("%li\n", p_regist->id );
+        puts(p_regist->name);
+        puts(p_regist->last_name);
+        puts(p_regist->email);
+        puts(p_regist->b_type);
+        p_regist++;
         i++;
     }
 }
